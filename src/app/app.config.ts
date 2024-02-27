2db412f82be22598ad2396106b69cc6ef654fafd
gaburra-m <gaburra.m@gmail.com>
2024-02-27 17:22:09 -0600

    view transitions done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(
+      routes,
+      withViewTransitions({
+        skipInitialTransition: true, //no hace efecto al inciar
+        // onViewTransitionCreated(transitionInfo) {
+        //   console.log({ transitionInfo });
+        // },
+      })
+    ),
+  ],
 };
