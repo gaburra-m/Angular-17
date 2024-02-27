6fbc96b609206120c92627b503cd3463a3e7551c
gaburra-m <gaburra.m@gmail.com>
2024-02-27 17:01:17 -0600

    defer options - triggers done

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,9 +1,11 @@
+import { HeavyLoadersFastComponent } from '@/app/shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeavyLoadersFastComponent, TitleComponent],
   templateUrl: './defer-options.component.html',
   styles: ``,
 })
